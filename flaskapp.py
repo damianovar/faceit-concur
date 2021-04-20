1f0823c09c981801ce366947f4f2d89e04cd5534
Jafagervik <jorgenafagervik@gmail.com>
2021-04-20 18:41:44 +0200

    Fixed merge conflict

--- flaskapp.py
+++ flaskapp.py
@@ -114,32 +114,38 @@ def allowed_file(filename):
     else:
         return False
 
+
 @app.route("/graphviz/<sheet>/")
 def graphs(sheet):
-    #ss.upload_CU_file("filename")
-    #lists = ss.read_course_category_tree("linear-algebra", "specification of the content units' hierarchies", 4)
-    #lists = ss.read_course_category_tree("Course_content_signals", "specification of the content un", 4)
-    #print("Lists:",lists)
-    #print()
-    #nodes, edges = vis.get_nodes_and_edges_cu_hierarchies(lists)
-    #print()
-    #lists = ss.read_course_category_tree("testing", "specification of the content un", 4)
+    # ss.upload_CU_file("filename")
+    # lists = ss.read_course_category_tree("linear-algebra", "specification of the content units' hierarchies", 4)
+    # lists = ss.read_course_category_tree("Course_content_signals", "specification of the content un", 4)
+    # print("Lists:",lists)
+    # print()
+    # nodes, edges = vis.get_nodes_and_edges_cu_hierarchies(lists)
+    # print()
+    # lists = ss.read_course_category_tree("testing", "specification of the content un", 4)
     lists = ss.read_course_category_tree(sheet, "content units hierarchies", 5)
-    #print("Lists:",lists)
-    #print()
+    # print("Lists:",lists)
+    # print()
     nodes, edges = vis.get_nodes_and_edges_cu_hierarchies(lists)
     print()
     print("Nodes", nodes, "\n")
     print("Edges:", edges, "\n")
-    #print()
-    #print("Values:",nodes)
-    return render_template("graphviz.html", title='Visualize graphs',nodes=nodes, edges=edges)
+    # print()
+    # print("Values:",nodes)
+    return render_template(
+        "graphviz.html", title="Visualize graphs", nodes=nodes, edges=edges
+    )
+
 
 @app.route("/graphlist")
 def graph_list():
     ss.delete_all_files()
     available_CU_files = ss.get_available_CU_files()
-    return render_template("graphlist.html", title="Graph list", CU_files=available_CU_files)
+    return render_template(
+        "graphlist.html", title="Graph list", CU_files=available_CU_files
+    )
 
 
 @app.route("/upload_excel", methods=["GET", "POST"])
@@ -157,10 +163,8 @@ def upload_excel():
             else:
                 print("That file extension is not allowed")
                 return redirect(request.url)
-            
+
     return render_template("upload_excel.html", title="Upload Excel")
-
-
 
 
 @app.route("/upload", methods=["GET", "POST"])
@@ -185,93 +189,91 @@ def upload_tex():
 
 @app.route("/submit_answer", methods=["GET", "POST"])
 @login_requiered
-
-def submit_answer():
-    """Check if user answered a question and redirect accordingly."""
-    data, selection_data = db.list_question_objects_lite()
-    if request.method == "POST":
-        selection = request.form.get("id")
-        if selection:
-            answer = request.form.get("answer")
-            question_obj = db.get_question_by_obj_id(selection)
-            db.write_answer_to_mongo(question_obj, answer)
-
-            return render_template(
-                "answer_submitted_successfully.html",
-                answer=answer,
-                question=question_obj.question,
-            )
-        else:
-            return render_template(
-                "no_question_selected.html", title="Answer not submitted"
-            )
-
-    return render_template(
-        "submit_answer.html",
-        title="Submit Answer",
-        data=data,
-        selection_data=selection_data,
-    )
-
 def show_questions():
     data, selection_data = db.list_question_objects()
 
     if request.method == "POST":
-        selected_question = request.form.get('question_button')
+        selected_question = request.form.get("question_button")
         messages = json.dumps({"selected_question_id": selected_question})
-        return redirect(url_for('answer_selected_question', messages=messages))
+        return redirect(url_for("answer_selected_question", messages=messages))
 
-    return render_template("submit_answer/question_list.html", data=data, selection_data=selection_data)
-
+    return render_template(
+        "submit_answer/question_list.html", data=data, selection_data=selection_data
+    )
 
 
 @app.route("/submit_answer/answer_selected_question", methods=["GET", "POST"])
 @login_requiered
 def answer_selected_question():
-    messages = request.args['messages']
-    selected_question_id = json.loads(messages)['selected_question_id']
+    messages = request.args["messages"]
+    selected_question_id = json.loads(messages)["selected_question_id"]
 
     if request.method == "POST":
-        selected_multiple_choice_answer = request.form.get('id')
+        selected_multiple_choice_answer = request.form.get("id")
         if not selected_multiple_choice_answer:
-            return render_template("submit_answer/error_no_answer_selected.html", title='Answer not submitted')
+            return render_template(
+                "submit_answer/error_no_answer_selected.html",
+                title="Answer not submitted",
+            )
 
-        question_id = request.form.get('multiple_choice_button')
-        written_answer = request.form.get('written_answer')
-        perceived_difficulty = request.form.get('rating')
+        question_id = request.form.get("multiple_choice_button")
+        written_answer = request.form.get("written_answer")
+        perceived_difficulty = request.form.get("rating")
 
-        messages = json.dumps({"selected_multiple_choice_answer": selected_multiple_choice_answer, 
-                                "question_id": question_id, 
-                                "written_answer": written_answer, 
-                                "perceived_difficulty": perceived_difficulty})
+        messages = json.dumps(
+            {
+                "selected_multiple_choice_answer": selected_multiple_choice_answer,
+                "question_id": question_id,
+                "written_answer": written_answer,
+                "perceived_difficulty": perceived_difficulty,
+            }
+        )
 
-        return redirect(url_for('show_submission_info', messages=messages))
+        return redirect(url_for("show_submission_info", messages=messages))
 
     selected_question_obj = db.get_question_by_obj_id(selected_question_id)
-    list_of_options, idx_list_for_options = db.get_answer_options_from_question_obj(selected_question_obj)
+    list_of_options, idx_list_for_options = db.get_answer_options_from_question_obj(
+        selected_question_obj
+    )
     question_image = db.get_question_image(selected_question_obj.id)
 
     current_user_role = db.get_user_role()
-    if current_user_role == 'Admin' or current_user_role == 'Teacher':
-        correct_answer = "The correct answer is: " + selected_question_obj.correct_answer[0]
-    else: # current_user_role == 'Student'
+    if current_user_role == "Admin" or current_user_role == "Teacher":
+        correct_answer = (
+            "The correct answer is: " + selected_question_obj.correct_answer[0]
+        )
+    else:  # current_user_role == 'Student'
         correct_answer = None
 
-    return render_template("submit_answer/selected_question_page.html", data=list_of_options, selection_data=idx_list_for_options, question_id = selected_question_obj.id, 
-                            question_text = selected_question_obj.question, correct_answer=correct_answer, question_image=question_image)
+    return render_template(
+        "submit_answer/selected_question_page.html",
+        data=list_of_options,
+        selection_data=idx_list_for_options,
+        question_id=selected_question_obj.id,
+        question_text=selected_question_obj.question,
+        correct_answer=correct_answer,
+        question_image=question_image,
+    )
 
 
 @app.route("/submit_answer/successfully_submitted", methods=["GET"])
 @login_requiered
 def show_submission_info():
-    messages = request.args['messages']
-    selected_multiple_choice_answer = json.loads(messages)['selected_multiple_choice_answer']
-    question_id = json.loads(messages)['question_id'] 
-    written_answer = json.loads(messages)['written_answer'] 
-    perceived_difficulty = json.loads(messages)['perceived_difficulty'] 
+    messages = request.args["messages"]
+    selected_multiple_choice_answer = json.loads(messages)[
+        "selected_multiple_choice_answer"
+    ]
+    question_id = json.loads(messages)["question_id"]
+    written_answer = json.loads(messages)["written_answer"]
+    perceived_difficulty = json.loads(messages)["perceived_difficulty"]
 
     answered_question_obj = db.get_question_by_obj_id(question_id)
-    db.write_answer_to_mongo(answered_question_obj, written_answer, selected_multiple_choice_answer, perceived_difficulty)
+    db.write_answer_to_mongo(
+        answered_question_obj,
+        written_answer,
+        selected_multiple_choice_answer,
+        perceived_difficulty,
+    )
 
     options_list = answered_question_obj.options
     display_answer = str(options_list[int(selected_multiple_choice_answer)])
@@ -280,7 +282,14 @@ def show_submission_info():
     info_plot = db.make_bar_plot(data)
     perceived_difficulty = db.get_avg_perceived_difficulty(question_id)
 
-    return render_template("submit_answer/answer_submitted_successfully.html", answer=display_answer,question=answered_question_obj.question, plot=info_plot, perceived_difficulty=perceived_difficulty)
+    return render_template(
+        "submit_answer/answer_submitted_successfully.html",
+        answer=display_answer,
+        question=answered_question_obj.question,
+        plot=info_plot,
+        perceived_difficulty=perceived_difficulty,
+    )
+
 
 @app.route("/get-tex/<tex_name>", methods=["GET", "POST"])
 def get_image(tex_name):
@@ -304,8 +313,8 @@ def get_image(tex_name):
 def downloads():
 
     """Let a user download questions."""
-    
-  # Check one which is used - 1 or 2
+
+    # Check one which is used - 1 or 2
     data, selection_data = db.list_question_objects()
 
     if request.method == "POST":
@@ -424,5 +433,3 @@ def add_headers(response):
 
 if __name__ == "__main__":
     app.run()
-
-
